fad9248b098e1a6367ef7bf1c290d9fbfbd0f4ba
Mikealeee <michael.yewchuan@gmail.com>
2023-02-28 21:13:38 +0800

    Add search UI and basic query from solr

--- frontend/info_retrieval_ui/src/App.js
+++ frontend/info_retrieval_ui/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useHttpClient } from './hook';
+import { useState, useEffect } from "react";
+import { Input, Space } from 'antd';
 
 function App() {
+
+  const { sendRequest } = useHttpClient();
+  const[SolrData, setSolrData] = useState('');
+
+  const fetchSolrData = async () => {
+    let url = "http://localhost:8983/solr/movie_db/query?indent=true&q.op=OR&q=body%3Abatman&useParams=";
+
+    const response = await sendRequest(url);
+    console.log(response);
+    // console.log(response);
+    setSolrData(response.jobs);
+}
+
+const { Search } = Input;
+
+
+const onSearch = (value) => fetchSolrData();;
+
+// useEffect(() => {
+//   fetchJobs();
+// }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Search placeholder="input search text" style={{ width: 304 }} onSearch={onSearch} enterButton />
+
+
     </div>
   );
 }
