990c4a9bb63722d401fbe2e8e5c3dcaa2397be07
Mikealeee <michael.yewchuan@gmail.com>
2023-03-01 21:52:20 +0800

    Update search and add README for solr

--- frontend/info_retrieval_ui/src/App.js
+++ frontend/info_retrieval_ui/src/App.js
@@ -2,37 +2,66 @@ import logo from './logo.svg';
 import './App.css';
 import { useHttpClient } from './hook';
 import { useState, useEffect } from "react";
-import { Input, Space } from 'antd';
+import { Button, Input, List, Typography } from 'antd';
 
 function App() {
 
   const { sendRequest } = useHttpClient();
-  const[SolrData, setSolrData] = useState('');
+  const[SolrData, setSolrData] = useState([]);
+  const[searchInput, setSearchInput] = useState('');
 
-  const fetchSolrData = async () => {
-    let url = "http://localhost:8983/solr/movie_db/query?indent=true&q.op=OR&q=body%3Abatman&useParams=";
+  const fetchSolrData = async (searchTerm) => {
 
+    let url = "http://localhost:8983/solr/movie_db/query?indent=true&q.op=OR&useParams=&q=body%3A" + searchTerm //+ "%22"; //'//"body%3Atoys%20body%3Astory"
+    
+    // const searchTermArray = searchTerm.split(" ");
+    // let i = 0;
+    // console.log(searchTermArray, searchTermArray.length)
+    // while (i < searchTermArray.length) {
+    //   url = url + ("body%3A" + searchTerm + "%20");
+    //   console.log("ss", url)
+    //   i++;
+    // }
+    
     const response = await sendRequest(url);
-    console.log(response);
-    // console.log(response);
-    setSolrData(response.jobs);
+    return response;
 }
 
 const { Search } = Input;
 
+const onSearch = (searchTerm) => {
+  const solrData = fetchSolrData(searchTerm).then((data) => {
+    console.log(data.response.docs);
+    setSolrData(data.response)
+    console.log(searchTerm)
+  });
+ }
 
-const onSearch = (value) => fetchSolrData();;
+ const onChange = (e) => {
+  setSearchInput(e.target.value);
+};
 
-// useEffect(() => {
-//   fetchJobs();
-// }, []);
-  
+useEffect(() => {
+  fetchSolrData();
+}, []);
+
   return (
     <div className="App">
 
-    <Search placeholder="input search text" style={{ width: 304 }} onSearch={onSearch} enterButton />
+    <h1>Movies</h1>
+    
+    <Search placeholder="input search text" style={{ width: 304 }} value={searchInput} onChange={onChange} onSearch={() => onSearch(searchInput)} enterButton />
+     <List
 
+      bordered
 
+      dataSource={SolrData.docs}
+      renderItem={(item) => (
+        <List.Item>
+          <Typography.Text mark> </Typography.Text> {item.body}
+        </List.Item>
+      )}
+      />
     </div>
   );
 }
