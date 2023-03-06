4a4caa731ba1308ab3f23b6f5337f61ba0009653
Mikealeee <michael.yewchuan@gmail.com>
2023-03-07 02:56:09 +0800

    Add advanced query on movie name, update spellcheck

--- frontend/info_retrieval_ui/src/App.js
+++ frontend/info_retrieval_ui/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useHttpClient } from './hook';
 import { useState, useEffect} from "react";
-import { Card, Input, List, Image, Button } from 'antd';
+import { Card, Input, List, Image, Button, Select } from 'antd';
 import moviePicture from "./pictures/moviePicture.png";
 
 function App() {
@@ -11,46 +11,92 @@ function App() {
   const[SolrData, setSolrData] = useState([]);
   const[SpellCheck, setSpellCheck] = useState('');
   const[searchInput, setSearchInput] = useState('');
+  const[MovieList, setMovieList]  = useState('');
+  const[MovieNameInput, setMovieNameInput] = useState("*");
+  const[ShowAdvancedSearch, setShowAdvancedSearch] = useState(0);
+  const { Search } = Input;
+  
+  // fetches reddit comments based on users search
+  const fetchSolrData = async (searchTerm, movieName) => {
 
-  const fetchSolrData = async (searchTerm) => {
-
-    let url = "http://localhost:8983/solr/movie_db/spell?indent=true&q.op=OR&rows=20&useParams=&q=body:(" + searchTerm + ")" //+ "%22"; //'//"body%3Atoys%20body%3Astory"
-    
-    // const searchTermArray = searchTerm.split(" ");
-    // let i = 0;
-    // console.log(searchTermArray, searchTermArray.length)
-    // while (i < searchTermArray.length) {
-    //   url = url + ("body%3A" + searchTerm + "%20");
-    //   console.log("ss", url)
-    //   i++;
-    // }
-    
+    let url = "http://localhost:8983/solr/movie_db/spell?indent=true&q.op=AND&rows=20&useParams=&q=body:(" + searchTerm + ")movie_name:(" + movieName + ")" //+ "%22"; //'//"body%3Atoys%20body%3Astory"
+    console.log(url);
     const response = await sendRequest(url);
     console.log(response);
     return response;
-}
+  }
 
-const { Search } = Input;
+  // fetches all movie titles in database
+  const fetchMovieData = async () => {
 
-const onSearch = (searchTerm) => {
-  const solrData = fetchSolrData(searchTerm).then((data) => {
-    setSolrData(data.response);
-    setSpellCheck(data.spellcheck.collations);
-    console.log(data.response.docs);
-    //console.log(SpellCheck[3].collationQuery)
-  });
- }
+    let url = "http://localhost:8983/solr/movie_db/query?indent=true&q.op=AND&rows=3000&useParams=&q=*:*&fl=movie_name" //+ "%22"; //'//"body%3Atoys%20body%3Astory"
 
- const onChange = (e) => {
-  setSearchInput(e.target.value);
-};
+    const response = await sendRequest(url);
+    console.log(response);
 
-useEffect(() => {
-  fetchSolrData();
-}, []);
+    const options = new Set();
+    const movies = [{
+      value:"*",
+      label: "None",}]
+    for (let i = 0; i < response.response.docs.length; i++){
+      if (options.has(response.response.docs[i].movie_name[0])) {
+          continue;
+      }
+      else{
+        options.add(response.response.docs[i].movie_name[0])
+        movies.push({
+          value: response.response.docs[i].movie_name[0],
+          label: response.response.docs[i].movie_name[0],})
+      }
+    }
+
+    setMovieList(movies);
+    return response;
+  }
+
+  // calls fetching of reddit comments upon clicking search icon
+  const onSearch = (searchTerm, movieName) => {
+    const solrData = fetchSolrData(searchTerm, movieName).then((data) => {
+      setSolrData(data.response);
+      setSpellCheck(data.spellcheck.collations);
+      console.log(data.response.docs);
+      console.log("Spellcheck", SpellCheck);
+    });
+  }
+
+  // updates search bar 
+  const onChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  // button to toggle advanced search (by movie name so far)
+  const toggleAdvancedSearch = () => {
+    setShowAdvancedSearch(!ShowAdvancedSearch);
+    fetchMovieData();
+    setMovieNameInput("*");
+    //console.log("Advanced search:", ShowAdvancedSearch);
+  };
+
+  // updates advanced search:movie_name 
+  const handleChange = (value) => {
+    if (value) {
+      setSearchInput("*");
+      setMovieNameInput(value);
+    }
+    else{
+      setMovieNameInput("*");
+      setSearchInput("");
+    }
+    console.log(`Selected: ${value}`);
+  };
+
+  useEffect(() => {
+    fetchSolrData();
+    fetchMovieData();
+  }, []);
 
   return (
-    <div className="App" >
+    <div className="App">
       
       <Image
       style={{paddingTop: 30, borderRadius: 40}}
@@ -60,14 +106,29 @@ useEffect(() => {
       />
 
       <h1 style={{fontSize: 60}}>Movies</h1>
-      <div > 
-        <Search placeholder="input search text" style={{ width: 700, }} value={searchInput} onChange={onChange} onSearch={() => onSearch(searchInput)} enterButton />
-      </div>
-      <br/>
-      <div > 
-        <Button style={{width: 250}}> {(SolrData.numFound < 5) ?  (SpellCheck.length == 0 && searchInput != "") ? "Found what you were looking for?" : "Did you mean " + SpellCheck[1].collationQuery.slice(5, ).replace("(", "").replace(")", "") : "Found what you were looking for?"  }</Button> 
-      </div>
+
+      <Search placeholder="input search text" style={{ width: 500, }} value={searchInput} onChange={onChange} onSearch={() => onSearch(searchInput, MovieNameInput)} enterButton />
+
+      <text style={{width: 250, padding: 10}}> {(SolrData.numFound < 5) ?  (SpellCheck.length == 0 ) ? "" : "Did you mean " + SpellCheck[1].collationQuery.slice(5, ).replace("(", "").replace(")", "").split('movie_name')[0]: ""  }</text> 
+   
+      <Button style={{marginBottom: 10}} onClick = {() => toggleAdvancedSearch()}> Advanced search </Button>
       
+      {(ShowAdvancedSearch) ?
+          <Select
+            style={{width: 200}}
+            showSearch 
+            bordered
+            allowClear
+            placeholder="Movie name"
+            value={MovieNameInput}
+            onClear = {() => setMovieNameInput("*")}
+            onChange={handleChange}
+            options={MovieList} 
+          /> 
+          : 
+          <></>
+      }
+  
       <List
         style={{ marginTop: 50, }}
         dataSource={SolrData.docs}
@@ -98,7 +159,6 @@ useEffect(() => {
 }
 
 export default App;
-
 
 // <List 
 // dataSource={SpellCheck}
