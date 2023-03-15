60abcabb37369e5fddb1c9a89e83814a62e5ae8a
heavy-syzygy <88018628+heavy-syzygy@users.noreply.github.com>
2023-03-15 19:41:55 +0800

    updated textblob folder to include accuracy score

---
+++ classification/textblob/textblob_model.py
@@ -0,0 +1,90 @@
+#import the library
+from textblob import TextBlob
+import pandas as pd
+
+from sklearn.metrics import accuracy_score
+
+import warnings
+warnings.filterwarnings("ignore")
+
+#import dataset
+df = pd.read_csv("uncleaned_data.csv")
+df = df.dropna(subset=['subjectivity', 'polarity', 'relevance'])
+
+#data info
+print("Subjectivity count:")
+print(df["subjectivity"].value_counts())
+print(df["subjectivity"].value_counts(normalize=True), "\n")
+
+print("Polarity count:")
+print(df["polarity"].value_counts())
+print(df["polarity"].value_counts(normalize=True), "\n")
+
+print("Relevance count:")
+print(df["relevance"].value_counts())
+print(df["relevance"].value_counts(normalize=True), "\n")
+
+df_drop_useless = df[df['relevance']==0]
+
+print("Relevance count (drop useless data):")
+print(df_drop_useless["relevance"].value_counts())
+print(df_drop_useless["relevance"].value_counts(normalize=True), "\n")
+
+#Label sentiment and subjectivity using TextBlob
+def sentiment_analysis(data):
+    def getSubjectivity(text):
+        return TextBlob(text).sentiment.subjectivity
+
+    #Create a function to get the polarity
+    def getPolarity(text):
+        return TextBlob(text).sentiment.polarity
+
+    #Create two new columns ‘Subjectivity’ & ‘Polarity’
+    data['TB_Subj_Score'] = data['body'].apply(getSubjectivity)
+    data['TB_Pol_Score'] = data['body'].apply(getPolarity)
+
+    def getSubjAnalysis(score):
+        if score < 0.5:
+            return 0 #‘Objective’
+        else:
+            return 1 #‘Subjective’
+
+    def getPolAnalysis(score):
+        if score < 0:
+            return 0 #‘Negative’
+        #elif score == 0:
+        #return ‘Neutral’
+        else:
+            return 1 #‘Positive’
+
+    data['TB_Subjectivity'] = data['TB_Subj_Score'].apply(getSubjAnalysis)
+    data['TB_Polarity'] = data['TB_Pol_Score'].apply(getPolAnalysis)
+
+    return data
+
+df_TB_labelled = sentiment_analysis(df_drop_useless)
+
+#data info of labelled Textblob data
+print("Subjectivity count  (Textblob):")
+print(df_TB_labelled["TB_Subjectivity"].value_counts())
+print(df_TB_labelled["TB_Subjectivity"].value_counts(normalize=True), "\n")
+
+print("Polarity count (Textblob):")
+print(df_TB_labelled["TB_Polarity"].value_counts())
+print(df_TB_labelled["TB_Polarity"].value_counts(normalize=True), "\n")
+
+#Compare accuracy Textblob vs manual
+print("Accuracy Textblob vs Manual:")
+print("Subjectivity Acc: ", accuracy_score(df_TB_labelled["subjectivity"],
+                                           df_TB_labelled["TB_Subjectivity"],
+                                           normalize=False))
+print("Subjectivity Acc %: ", accuracy_score(df_TB_labelled["subjectivity"],
+                                           df_TB_labelled["TB_Subjectivity"]))
+print("Polarity Acc: ", accuracy_score(df_TB_labelled["polarity"],
+                                           df_TB_labelled["TB_Polarity"],
+                                           normalize=False))
+print("Polarity Acc %: ", accuracy_score(df_TB_labelled["polarity"],
+                                           df_TB_labelled["TB_Polarity"]))
+
+#Save TextBlob labelled data to csv
+df_TB_labelled.to_csv("uncleaned_data_with_TextBlob.csv")
